0c8d071ab218c3dd004bc3c3586154713cc6593d
toktar <renata.toktar@dsr-corporation.com>
2022-11-21 16:15:57 +0300

    Fix linter

--- app/encoding.go
+++ app/encoding.go
@@ -1,12 +1,12 @@
 package app
 
 import (
+	"canow-chain/app/params"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/std"
 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
-
-	"canow-chain/app/params"
 )
 
 // makeEncodingConfig creates an EncodingConfig for an amino based test configuration.
