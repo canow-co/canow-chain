dc764c2a2be3b427ec063b47d8bd8bc3247de11f
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2022-11-23 19:21:03 +0500

    Add testdata package

---
+++ tests/integration/testdata/accounts.go
@@ -0,0 +1,12 @@
+package testdata
+
+const (
+	BASE_ACCOUNT_1             = "base_account_1"
+	BASE_ACCOUNT_2             = "base_account_2"
+	BASE_VESTING_ACCOUNT       = "base_vesting_account"
+	CONTINUOUS_VESTING_ACCOUNT = "continuous_vesting_account"
+	DELAYED_VESTING_ACCOUNT    = "delayed_vesting_account"
+	PERIODIC_VESTING_ACCOUNT   = "periodic_vesting_account"
+	BASE_ACCOUNT_1_ADDRESS     = "canow1xx3k0c86g9hclfh26u6gdjmdfkp75tvt3qa4mn"
+	BASE_ACCOUNT_2_ADDRESS     = "canow1p0xxlce6mvzh4kpdq06szr5z5uxrp9qx5gdt4k"
+)
