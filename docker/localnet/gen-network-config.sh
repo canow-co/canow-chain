ad9583c5b2fc88c8fba982de48bc943762852c33
toktar <renata.toktar@dsr-corporation.com>
2023-01-13 17:10:08 +0300

    fix: linter

--- docker/localnet/gen-network-config.sh
+++ docker/localnet/gen-network-config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2086
 
 # Generates network configuration for an arbitrary amount of validators, observers, and seeds.
 
@@ -38,16 +39,16 @@ function configure_node() {
   APP_TOML="${NODE_HOME}/config/app.toml"
   CONFIG_TOML="${NODE_HOME}/config/config.toml"
 
-  sed -i "$SED_EXT" 's/minimum-gas-prices = ""/minimum-gas-prices = "25ncheq"/g' "${APP_TOML}"
-  sed -i "$SED_EXT" 's/enable = false/enable = true/g' "${APP_TOML}"
+  sed -i $SED_EXT 's/minimum-gas-prices = ""/minimum-gas-prices = "25ncheq"/g' "${APP_TOML}"
+  sed -i $SED_EXT 's/enable = false/enable = true/g' "${APP_TOML}"
 
-  sed -i "$SED_EXT" 's|laddr = "tcp://127.0.0.1:26657"|laddr = "tcp://0.0.0.0:26657"|g' "${CONFIG_TOML}"
-  sed -i "$SED_EXT" 's|addr_book_strict = true|addr_book_strict = false|g' "${CONFIG_TOML}"
+  sed -i $SED_EXT 's|laddr = "tcp://127.0.0.1:26657"|laddr = "tcp://0.0.0.0:26657"|g' "${CONFIG_TOML}"
+  sed -i $SED_EXT 's|addr_book_strict = true|addr_book_strict = false|g' "${CONFIG_TOML}"
 
-  sed -i "$SED_EXT" 's/timeout_propose = "3s"/timeout_propose = "500ms"/g' "${CONFIG_TOML}"
-  sed -i "$SED_EXT" 's/timeout_prevote = "1s"/timeout_prevote = "500ms"/g' "${CONFIG_TOML}"
-  sed -i "$SED_EXT" 's/timeout_precommit = "1s"/timeout_precommit = "500ms"/g' "${CONFIG_TOML}"
-  sed -i "$SED_EXT" 's/timeout_commit = "5s"/timeout_commit = "500ms"/g' "${CONFIG_TOML}"
+  sed -i $SED_EXT 's/timeout_propose = "3s"/timeout_propose = "500ms"/g' "${CONFIG_TOML}"
+  sed -i $SED_EXT 's/timeout_prevote = "1s"/timeout_prevote = "500ms"/g' "${CONFIG_TOML}"
+  sed -i $SED_EXT 's/timeout_precommit = "1s"/timeout_precommit = "500ms"/g' "${CONFIG_TOML}"
+  sed -i $SED_EXT 's/timeout_commit = "5s"/timeout_commit = "500ms"/g' "${CONFIG_TOML}"
 }
 
 function configure_genesis() {
@@ -60,7 +61,7 @@ function configure_genesis() {
   GENESIS_TMP="${NODE_HOME}/config/genesis_tmp.json"
 
   # Default denom
-  sed -i "$SED_EXT" 's/"stake"/"ncheq"/' "${GENESIS}"
+  sed -i $SED_EXT 's/"stake"/"ncheq"/' "${GENESIS}"
 
   # Test accounts
   BASE_ACCOUNT_1="canow1xx3k0c86g9hclfh26u6gdjmdfkp75tvt3qa4mn"
